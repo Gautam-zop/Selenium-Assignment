4c880df63a5e61baf2a6359a47c90efb07cbe48e
GautamZOP <gautam.gandhi@kroger.com>
2021-10-27 16:14:40 +0530

    refactor

--- src/test/java/ui/page/CheckoutPage.java
+++ src/test/java/ui/page/CheckoutPage.java
@@ -3,8 +3,6 @@ package ui.page;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import ui.map.CheckoutMap;
 
 import java.util.List;
